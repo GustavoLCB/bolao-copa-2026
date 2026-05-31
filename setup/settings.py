02b8dd3866f87a9b28a1495994536b0bc27eb174
gustavolcbarbosa <gus.lopescb@gmail.com>
2026-05-31 12:18:44 -0300

    Arquivos finais: regras de premiacao, bolao privado e CSS do admin

--- setup/settings.py
+++ setup/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
